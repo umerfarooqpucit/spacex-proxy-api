a7dd29ffd785aaedd34d017083b642b15ede9bcd
Umer Farooq <umerfarooq.pucit@gmail.com>
2023-04-30 16:01:25 +0500

    added intital data models

---
+++ SpaceXProxyAPI/SpaceXProxyAPI/Data/Models/LaunchMetadata.cs
@@ -0,0 +1,44 @@
+﻿// <copyright file="LaunchMetadata.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace SpaceXProxyAPI.Data.Models
+{
+    using System.Text.Json.Serialization;
+
+    /// <summary>
+    /// Launch Metadata model class.
+    /// </summary>
+    public class LaunchMetadata
+    {
+        /// <summary>
+        /// Gets or sets Flight number.
+        /// </summary>
+        [JsonPropertyName("flight_number")]
+        public long FlightNumber { get; set; }
+
+        /// <summary>
+        /// Gets or sets Mission Name.
+        /// </summary>
+        [JsonPropertyName("mission_name")]
+        public string MissionName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether upcoming is true or false; shows if it is a past or upcoming launch.
+        /// </summary>
+        [JsonPropertyName("upcoming")]
+        public bool Upcoming { get; set; }
+
+        /// <summary>
+        /// Gets or sets Launch year.
+        /// </summary>
+        [JsonPropertyName("launch_year")]
+        public string LaunchYear { get; set; } = null!;
+
+        /// <summary>
+        /// Gets or sets launch site data.
+        /// </summary>
+        [JsonPropertyName("launch_site")]
+        public LaunchSite LaunchSite { get; set; } = null!;
+    }
+}
---
+++ SpaceXProxyAPI/SpaceXProxyAPI/Data/Models/LaunchSite.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="LaunchSite.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace SpaceXProxyAPI.Data.Models
+{
+    using System.Text.Json.Serialization;
+
+    /// <summary>
+    /// Launch Site model class.
+    /// </summary>
+    public class LaunchSite
+    {
+        /// <summary>
+        /// Gets or sets SiteId.
+        /// </summary>
+        [JsonPropertyName("site_id")]
+        public string SiteId { get; set; } = null!;
+
+        /// <summary>
+        /// Gets or sets Site name.
+        /// </summary>
+        [JsonPropertyName("site_name")]
+        public string SiteName { get; set; } = null!;
+
+        /// <summary>
+        /// Gets or sets Site Full Name.
+        /// </summary>
+        [JsonPropertyName("site_name_long")]
+        public string SiteNameLong { get; set; } = null!;
+    }
+}
