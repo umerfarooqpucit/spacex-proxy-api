00346446e01a979db2093098a9a566fea45e36eb
Umer Farooq <umerfarooq.pucit@gmail.com>
2023-04-30 16:02:07 +0500

    added getAll endpoint with default response.

--- SpaceXProxyAPI/SpaceXProxyAPI/Controllers/LaunchesController.cs
+++ SpaceXProxyAPI/SpaceXProxyAPI/Controllers/LaunchesController.cs
@@ -1,15 +1,28 @@
-// <copyright file="LaunchesController.cs" company="PlaceholderCompany">
+﻿// <copyright file="LaunchesController.cs" company="PlaceholderCompany">
 // Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
 
-using Microsoft.AspNetCore.Mvc;
-
 namespace SpaceXProxyAPI.Controllers
 {
+    using Microsoft.AspNetCore.Mvc;
+    using SpaceXProxyAPI.Data.Models;
+
+    /// <summary>
+    /// Implements launch api endpoints.
+    /// </summary>
     [ApiController]
     [Route("api/[controller]")]
     public class LaunchesController : ControllerBase
     {
-        
+        /// <summary>
+        /// Gets all past and upcoming launches.
+        /// </summary>
+        /// <returns>A  representing the result of the asynchronous operation.</returns>
+        [HttpGet]
+        [Route(nameof(GetAll))]
+        public async Task<List<LaunchMetadata>> GetAll()
+        {
+            return new List<LaunchMetadata>();
+        }
     }
 }
