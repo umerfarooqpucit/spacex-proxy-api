0ec23d1ecc1c6df00402f64f4bbe4b9cd61f30d6
Umer Farooq <umerfarooq.pucit@gmail.com>
2023-04-30 14:40:48 +0500

    Scaffolding

---
+++ SpaceXProxyAPI/SpaceXProxyAPI/Controllers/LaunchesController.cs
@@ -0,0 +1,15 @@
+// <copyright file="LaunchesController.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace SpaceXProxyAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LaunchesController : ControllerBase
+    {
+        
+    }
+}
